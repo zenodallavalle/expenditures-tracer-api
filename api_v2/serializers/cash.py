86042f7b675117bc7c77651a9a4fdcf6077554c8
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-10-11 21:48:21 +0200

    added automatic date to cash

--- api_v2/serializers/cash.py
+++ api_v2/serializers/cash.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from main.models import Cash
 from .DateFilterSerializer import DateFilterSerializer
 
@@ -7,7 +8,14 @@ class CashSerializer(DateFilterSerializer):
         self.include_children = include_children
         super().__init__(*args, **kwargs)
 
+    reference_date = serializers.DateTimeField(required=False)
+
     class Meta:
         model = Cash
         fields = ['id', 'name', 'value', 'date',
                   'reference_date', 'income', 'db']
+
+    def create(self, validated_data):
+        if not validated_data.get('reference_date', None):
+            validated_data['reference_date'] = self.context['request'].min_date
+        return super().create(validated_data)
