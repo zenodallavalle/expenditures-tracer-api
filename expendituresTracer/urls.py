ca809ca1e8e9b91919c909812862cfee8299964e
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-10-15 15:58:34 +0200

    deprecated url

--- expendituresTracer/urls.py
+++ expendituresTracer/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import include, path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/v1/', include('v1_api.urls')),
     path('api/v2/', include('api_v2.urls')),
-    path('', include('main.urls'))
 ]
