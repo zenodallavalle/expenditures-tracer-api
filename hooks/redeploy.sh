7c07ecb5fb8ec644d65db3394e5d33d2ce24b01e
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-11-06 15:55:28 +0100

    updated redeploy script

--- hooks/redeploy.sh
+++ hooks/redeploy.sh
@@ -2,3 +2,8 @@
 
 cd /var/www/vhosts/expenditures-tracer-api/
 git pull -f origin master
+. env/bin/activate
+rm -rf static/
+python manage.py collectstatic
+deactivate
+sudo service apache2 restart
