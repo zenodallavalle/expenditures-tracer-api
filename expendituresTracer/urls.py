c6e90448685d67e569cdc901529301ea36775702
Zeno Dalla Valle <zenodallavalle@gmail.com>
2021-11-22 09:37:26 +0100

    dropped api_v1 files

--- expendituresTracer/urls.py
+++ expendituresTracer/urls.py
@@ -19,6 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    # path('v1/', include('v1_api.urls')),
     path('v2/', include('api_v2.urls')),
 ]
--- v1_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class V1ApiConfig(AppConfig):
-    name = 'v1_api'
