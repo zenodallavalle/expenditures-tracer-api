00494c1732f13752b6d8b3329317591244ef8923
Zeno Dalla Valle <zenodallavalle@gmail.com>
2022-03-19 13:26:57 +0100

    fix auth bug

--- api_v2/permissions.py
+++ api_v2/permissions.py
@@ -3,10 +3,10 @@ from rest_framework import permissions
 
 class UserPermission(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
-        return True
+        return request.user.is_authenticated
 
     def has_permission(self, request, view):
-        return True
+        return request.user.is_authenticated
 
 
 class DBPermission(permissions.IsAuthenticated):
