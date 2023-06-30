ba5ce4734ad361dfe30b7ac8c9496684292ddcdb
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-06-30 15:59:52 +0200

    changed redeploy logic

--- hooks/redeploy.sh
+++ hooks/redeploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-ssh-keyscan github.com >> ~/.ssh/known_hosts
 cd /var/www/vhosts/expenditures-tracer-api/
 git pull -f origin master
 . env/bin/activate
-rm -rf static/
-python manage.py collectstatic --settings "expendituresTracer.production_settings"
+
 python manage.py migrate --settings "expendituresTracer.production_settings"
+python manage.py collectstatic --no-input --settings "expendituresTracer.production_settings"
+
 deactivate
 sudo service apache2 restart
