714ac1aaceb6b6985121511629961ea18aca212f
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-09-07 17:13:53 +0200

    bug fix

--- expendituresTracer/wsgi.py
+++ expendituresTracer/wsgi.py
@@ -15,7 +15,9 @@ from dotenv import dotenv_values
 sys.path.append("../")
 # adjust the Python version in the line below as needed
 sys.path.append(
-    dotenv_values(os.path.join(os.getcwd(), ".env"))["PYTHON_SITE_PACKAGES_PATH"]
+    dotenv_values(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env"))[
+        "PYTHON_SITE_PACKAGES_PATH"
+    ]
 )
 
 os.environ.setdefault(
