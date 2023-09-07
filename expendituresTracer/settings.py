de719e94076b3ec73974da0681428ac47dd0ce21
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-09-07 16:03:59 +0200

    added autofield option

--- expendituresTracer/settings.py
+++ expendituresTracer/settings.py
@@ -132,6 +132,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = "static/"
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": False,
