f455331f6c0d265366230cbb69493a69274342a8
Zeno Dalla Valle <zenodallavalle@gmail.com>
2023-09-07 17:17:59 +0200

    bug fix

--- expendituresTracer/wsgi.py
+++ expendituresTracer/wsgi.py
@@ -12,10 +12,10 @@ import sys
 from django.core.wsgi import get_wsgi_application
 from dotenv import dotenv_values
 
-sys.path.append("../")
+sys.path.append(os.path.join(os.getcwd, os.pardir))
 # adjust the Python version in the line below as needed
 sys.path.append(
-    dotenv_values(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env"))[
+    dotenv_values(os.path.join(os.getcwd, os.pardir, ".env"))[
         "PYTHON_SITE_PACKAGES_PATH"
     ]
 )
