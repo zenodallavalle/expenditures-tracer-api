2bf18121abb96fabca0379d0cdf753da8360bf48
Zeno Dalla Valle <zenodallavalle@gmail.com>
2024-11-12 14:12:16 +0100

    test webhooks

--- expendituresTracer/production_settings.py
+++ expendituresTracer/production_settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+# Test
+
 from pathlib import Path
 import os
 from dotenv import dotenv_values
