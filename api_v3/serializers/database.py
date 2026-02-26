d923b066fe8d7f5a84e8fab3617560863cdb5405
Zeno Dalla Valle <zenodallavalle@gmail.com>
2026-02-26 17:39:07 +0000

    bug fix

--- api_v3/serializers/database.py
+++ api_v3/serializers/database.py
@@ -119,27 +119,33 @@ class FullDatabaseSerializer(DateFilterSerializer):
         current_moneys = (
             instance.cashes.filter(income=False)
             .annotate(my=models.functions.TruncMonth("reference_date"))
-            .order_by("my")
+            .order_by("my", "-reference_date")
+            .distinct("my")
+            .values("my", "value")
         )
-        subquery = (
-            current_moneys.values("my")
-            .annotate(latest=models.Max("reference_date"))
-            .filter(pk=models.OuterRef("pk"))
-            .values("latest")
-        )
+        # current_moneys = (
+        #     instance.cashes.filter(income=False)
+        #     .annotate(my=models.functions.TruncMonth("reference_date"))
+        #     .order_by("my")
+        # )
+        # subquery = (
+        #     current_moneys.values("my")
+        #     .annotate(latest=models.Max("reference_date"))
+        #     .filter(pk=models.OuterRef("pk"))
+        #     .values("latest")
+        # )
 
-        current_moneys = (
-            current_moneys.annotate(latest=models.Subquery(subquery))
-            .values("my")
-            .annotate(
-                latest_value=models.Sum(
-                    "value",
-                    default=0,
-                    filter=models.Q(reference_date=models.F("latest")),
-                )
-            )
-            .values("my", "latest_value")
-        )
+        # current_moneys = (
+        #     current_moneys.annotate(latest=models.Subquery(subquery))
+        #     .annotate(
+        #         latest_value=models.Sum(
+        #             "value",
+        #             default=0,
+        #             filter=models.Q(reference_date=models.F("latest")),
+        #         )
+        #     )
+        #     .values("my", "latest_value")
+        # )
         months_available = set()
         incomes_dict = {}
         current_moneys_dict = {}
--- expendituresTracer/settings.py
+++ expendituresTracer/settings.py
@@ -164,8 +164,8 @@ CORS_ALLOWED_ORIGINS = []
 if RUN_MODE == "development":
     CORS_ALLOWED_ORIGINS.extend(
         [
-            "http://127.0.0.1:5173",
-            "http://localhost:5173",
+            "http://127.0.0.1:3000",
+            "http://localhost:3000",
         ]
     )
 CORS_ALLOWED_ORIGINS_ENV = os.environ.get(
