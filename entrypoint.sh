18f556ba5a615b6835be83331d2d683be99b047a
Zeno Dalla Valle <zenodallavalle@gmail.com>
2026-02-27 21:20:16 +0100

    fix: remove collectstatic command from entrypoint script for production mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ python manage.py migrate --no-input
 
 if [ "$EXPENDITURES_TRACER_API_RUN_MODE" = "development" ]; then
     echo "Running in development mode, collecting static files..."
-    python manage.py collectstatic --no-input
     echo "Starting server..."
     python manage.py runserver 0.0.0.0:8000 --nothreading
 else
